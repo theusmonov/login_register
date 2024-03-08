a6772a0380f7bd511a0b8ac937f408ba1cf7ea81
theusmon0v <asadbek.usmonoff2003@gmail.com>
2024-03-08 10:52:15 +0500

    databasega ulandi postgres ishlatiladi

---
+++ src/db/database.js
@@ -0,0 +1,7 @@
+import {Sequelize} from "sequelize";
+import {configration} from "../config/configFile.js";
+
+const {db} = configration
+export const sequelize = new Sequelize(db, {
+    logging: true
+})
